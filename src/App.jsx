f6693e524ef9a1651c5bf5a1af017e23fb999228
AmVaX <adrianmati@o2.pl>
2026-02-22 23:53:06 +0100

    Mapa4

--- src/App.jsx
+++ src/App.jsx
@@ -101,7 +101,7 @@ function Home() {
 
             <div className="interactive-map-container">
                 <iframe
-                    src="https://www.google.com/maps/d/edit?mid=1eSk8bzMUuHuczebPE8UWC5YhhYzjilQ&usp=sharing"
+                    src="https://www.google.com/maps/d/embed?mid=1eSk8bzMUuHuczebPE8UWC5YhhYzjilQ&ehbc=2E312F"
                     className="interactive-map"
                     title="Mapa punktów gry terenowej"
                     loading="lazy"
